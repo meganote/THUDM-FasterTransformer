55e4df7abe5f0430355ff9a8e236df0dcfb4c60e
xiaomao23zhi <gokasama@icloud.com>
2023-04-07 22:39:25 +0800

    Update docker_run.sh

--- scripts/docker_run.sh
+++ scripts/docker_run.sh
@@ -2,7 +2,7 @@ MP_SIZE=4
 DATA_TYPE=int4
 
 # Run server
-docker run -d -it --rm --shm-size=10g -p 5000:5000 \
+docker run -d -it --restart=always --shm-size=10g -p 5000:5000 \
   -v /var/cache/fscache/glm-130b/glm-130b-sat-${MP_SIZE}-${DATA_TYPE}/49300:/checkpoints:ro \
   -v /var/cache/fscache/glm-130b/ice_text.model:/root/.icetk_models/ice_text.model \
   -e MP_SIZE=${MP_SIZE} -e DATA_TYPE=${DATA_TYPE} \
